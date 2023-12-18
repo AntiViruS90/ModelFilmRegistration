b91ce35c669c51ff72e6ddf39d192e3102417b6c
AntiViruS90 <131804906+AntiViruS90@users.noreply.github.com>
2023-12-18 20:57:34 +0300

    adaptation CSS

--- SearchOnLine/settings.py
+++ SearchOnLine/settings.py
@@ -126,4 +126,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'slava-nikitin-1990@mail.ru'
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = 2525
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = 'slava-nikitin-1990@mail.ru'
+EMAIL_HOST_PASSWORD = 'zz7FH4urdse1Sf4PdW0c'
